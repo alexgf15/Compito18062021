6d06990ba73c9fe3c9e248b0145e2bf9d01b307e
alexgf15 <x81000962@studium.unict.it>
2021-06-27 16:43:45 +0200

    ho creato la classe coda e definito i metodi

--- main.cpp
+++ main.cpp
@@ -44,7 +44,8 @@ public:
     double getArea() const {return (base*altezza)/2;}
 };
 
-bool Shape::operator<(const Shape &rhs) const {
+bool Shape::operator<(const Shape &rhs) const
+{
     return this->getArea()<rhs.getArea();
 }
 bool Shape::operator>(const Shape &rhs) const {
@@ -60,6 +61,89 @@ bool Shape::operator>=(const Shape &rhs) const {
 bool Shape::operator==(const Shape &rhs) const {
     return this->getArea() == rhs.getArea();
 }
+
+template <class T> class Nodo
+{
+    public:
+    T valore;
+    Nodo * successivo;
+    Nodo(T val):valore(val),successivo(nullptr){}
+
+};
+
+template <class T> class Coda
+{
+ Nodo<T> *fronte, *fine;
+
+public:
+Coda(): fronte(nullptr),fine(nullptr){}
+
+void inserisci( T key);
+T preleva();
+bool isempty();
+
+template<class U>
+friend
+ostream & operator<<(ostream & out, const Coda<U> & c);
+};
+
+template<class T>void Coda<T>::inserisci(T val)
+{
+    Nodo<T> * nuovo= new Nodo<T>(val);
+    if(fine==nullptr)
+    {
+        fronte=fine=nuovo;
+    }
+    else
+    {
+    fine->successivo=nuovo;
+    fine=nuovo;
+    }
+
+}
+
+template<class T> T Coda<T>::preleva()
+{
+  Nodo<T> *temp=fronte;
+  fronte=fronte->successivo;
+
+  if(fronte==nullptr)
+    fine=fronte;
+
+  return temp->valore;
+}
+
+template<class T>bool Coda<T>::isempty()
+{
+    if((fronte==nullptr)&&(fine==nullptr))
+        return true;
+    else
+        return false;
+
+}
+
+
+template<class U>
+ostream & operator<<(ostream & out, const Coda<U> & c)
+{
+    Nodo<U> *temp=nullptr;
+
+    if(c.isempty())
+    {
+        out<<"La coda e' vuota"<<endl;
+    }
+
+    else
+    {
+        temp=c.preleva();
+        while(temp!=nullptr)
+        {
+            out<<*(temp->valore)<<endl;
+        }
+    }
+
+}
+
 
 
 int main()
